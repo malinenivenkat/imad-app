e3ac9c6fba191a45ece2ada76437eed52d0b2259
malinenivenkat <30699144+malinenivenkat@users.noreply.github.com>
2017-08-04 05:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendF
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/article-one',function(req,res)){
+    res.send('article one is requested and being srved');
+}};
+
+app.get('/article-one',function(req,res)){
+    res.send('article one is requested and being srved');
+    
+}};
+
+app.get('/article-one',function(req,res)){
+    res.send('article one is requested and being srved');
+}};
+    app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
