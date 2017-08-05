4f0fe6658ef1b587a0f928a08e3166e7cd793e60
malinenivenkat <30699144+malinenivenkat@users.noreply.github.com>
2017-08-05 19:37:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+// change the text of main text div
+var element=document.getElementById('main.text');
+elment.innerHTML='college management system-CSTAFF';
+// move the image
+var img=document.getElemeentById('modi');
+var marginleft=0;
+function moveRight(){
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+    
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveRight,50);
+    
+};
